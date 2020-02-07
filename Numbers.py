11a0aa6619f1fe1a4fd88d1e57583961a4b01314
AdegokeFawaz <54865336+AdegokeFawaz@users.noreply.github.com>
2020-02-07 09:25:36 -0600

    create numbers.py
    adding a description to the code

---
+++ Numbers.py
@@ -0,0 +1,18 @@
+#this code adds the first number and the second number
+first_number=4
+second_number=4
+print(first_number+second_number)
+#This code divides the first number and the second number
+first_number=16
+second_number=2
+print(first_number/second_number)
+#This code multiplies the first number and the second number
+first_number=4
+second_number=2
+print(first_number*second_number)
+#This code subtracts the first number and the second number
+first_number=16
+second_number=8
+print(first_number-second_number)
+Number(16)
+print(Number)
